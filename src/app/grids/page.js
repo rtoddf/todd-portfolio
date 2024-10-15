803e1bcb275280babf95a9755dcd2d2c59a36121
rtoddf <rtoddf@me.com>
2024-10-15 11:36:21 -0400

    figured out fetch for apis-todd

--- src/app/grids/page.js
+++ src/app/grids/page.js
@@ -1,31 +1,29 @@
-import axios from 'axios';
+// import axios from 'axios';
 import { StyledGrids } from '../styled/Grids.styled';
 
-function Page() {
-  const getData = () => {
-    axios
-      .get(
-        'http://itunes.apple.com/search?term=Charlie+Puth+Light+Switch&entity=musicTrack'
-      )
-      .then((response) => {
-        console.log('response: ', response.data);
-      })
-      .catch((err) => {
-        console.log('error: ', err);
-      });
+export default async function Page() {
+  const getData = async () => {
+    const response = await fetch(
+      `http://itunes.apple.com/search?term=Charlie+Puth+Light+Switch&entity=musicTrack`
+    );
+
+    return response.json();
   };
 
-  console.log('blip');
+  const data = await getData();
 
-  getData();
+  console.log('manny results: ', data?.results);
 
   return (
     <StyledGrids>
-      <h1>Grids 01</h1>
+      {data?.results.map((song) => {
+        return <p>{song?.trackName}</p>;
+      })}
+      {/* <h1>Grids 01</h1>
       <h1>Grids 02</h1>
-      <h1>Grids 03</h1>
+      <h1>Grids 03</h1> */}
     </StyledGrids>
   );
 }
 
-export default Page;
+// export default Page;
